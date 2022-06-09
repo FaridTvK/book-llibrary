d5777f2f0b0db2e89a5015c133aa9a16fb2e4891
faridTvK <faridztvki@gmail.com>
2022-06-09 11:53:27 +0200

    sec comment

---
+++ frontend/src/App.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+
+function App() {
+	const [books, setBooks] = useState([]);
+	const [searchText, setSearchText] = useState("");
+
+  
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const lookupBooks = async () => {
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ searchText })
+		};
+    console.log("das geht")
+		const response = await fetch('http://localhost:3003/searchbook', requestOptions);
+		const books = await response.json();
+		setBooks([...books]);
+	}
+
+	useEffect(() => {
+		lookupBooks();
+	}, []);
+
+	const handleButtonClick = () => {
+		lookupBooks();
+	}
+
+	return (
+		<div className="App">
+			{books.length === 0 && (
+				<div>Loading...</div>
+			)}
+			{books.length > 0 && (
+				<>
+					<input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+					<button onClick={handleButtonClick}>Search</button>
+					<div>
+						There are {books.length} books:
+					</div>
+					<ul>
+						{books.map((book, i) => {
+							return (
+								<>
+								<li key={i}>{book.title}</li>
+								<li> <input type="checkbox" onChange={(e)=>{}}/> </li>
+								</>
+							)
+						})}
+					</ul>
+				</>
+			)}
+		</div>
+	)
+}
+
+export default App
